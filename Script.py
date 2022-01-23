2aa774de868ea4c0d0838c0ca6f1d3988b56b689
Ssfive123 <91235488+Ssfive123@users.noreply.github.com>
2022-01-23 15:43:17 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,15 +1,16 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+    START_TXT = """<b>𝐇𝐄𝐘 {},
+⎆𝐈 𝐀𝐦 𝐚 𝐀𝐝𝐯𝐚𝐧𝐜𝐞𝐝 𝐟𝐢𝐥𝐭𝐞𝐫 𝐁𝐨𝐭.
+
+⎆𝐖𝐨𝐫𝐤𝐢𝐧𝐠 𝐖𝐢𝐭𝐡 <a href=https://t.me/MovieWibe>Movie Wibe 🎥</a></b>"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+    ABOUT_TXT = """<b>★ 𝖬ʏ 𝖭ᴀᴍᴇ :</b> {}
+<b>★ 𝖢ʀᴇᴀᴛᴏʀ </b>: <a href=https://t.me/MovieWibe>𝖬ᴏᴠɪᴇ 𝖶ɪʙᴇ 🎥</a>
+<b>★ 𝖫ᴀɴɢᴜᴀɢᴇ </b>: <code>Python3</code>
+<b>★ 𝖫ɪʙʀᴀʀʏ </b>: 𝖯ʏʀᴏɢʀᴀᴍ 𝖠sʏɴᴄɪᴏ
+<b>★ 𝖲ᴏᴜʀᴄᴇ 𝖢ᴏᴅᴇ </b>: <a href=https://t.me/+aYbIjDgZqY9lYjQ9>𝖢ʟɪᴄᴋ 𝖬ᴇ</a>
+"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
 - Source - https://github.com/EvamariaTG/EvaMaria  
